f300f98e09b5996914ba199b353c2ad158ffe3e7
PhantHive <phantomhive72@yahoo.fr>
2023-11-19 18:37:40 +0100

    feat: gradio link with nestjs

--- src/controllers/request.controller.ts
+++ src/controllers/request.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {Controller, Post, Body, Res, HttpStatus} from '@nestjs/common';
+export const importDynamic = new Function('modulePath', 'return import(modulePath)');
+const EventSource = require('eventsource');
+globalThis.EventSource = EventSource;
 
 @Controller('request')
 export class RequestController {
   @Post()
-  receiveString(@Body('data') data: string): string {
-    console.log(data); // This will log the received string to the console
-    return 'Request received!';
+  async receiveString(@Body('data') data: string, @Res() res): Promise<void> {
+    console.log('Received string: ', data);
+    const { client } = await importDynamic('@gradio/client');
+    const C = await client('https://phanthive-bigbrain.hf.space/--replicas/2glcb/', {})
+    const result = await C.predict("/predict", [data])
+    res.status(HttpStatus.OK).json({ prediction: result.data[0]})
   }
 }
