1c7952131869e7f3b3463e1f11e0dc3bef2020e3
PhantHive <phantomhive72@yahoo.fr>
2023-11-26 16:04:35 +0100

    fix: now auto-creating assets folder if not existing

--- deployPrivateFiles.sh
+++ deployPrivateFiles.sh
@@ -4,5 +4,10 @@
 SOURCE_DIR="/home/Phearion/aniaPrivate"
 DEST_DIR="$(pwd)/src/assets"
 
+# if the destination directory does not exist, create it
+if [ ! -d "$DEST_DIR" ]; then
+  mkdir -p "$DEST_DIR"
+fi
+
 # Copy files and directories recursively
 cp -a "$SOURCE_DIR"/* "$DEST_DIR"
