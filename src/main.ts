222c445e9925f32daddce38d04a2f88ffd3675b1
Zakaria <49699418+PhantHive@users.noreply.github.com>
2023-11-28 15:00:46 +0100

    feat: Gradio with NestJS (#2)
    * feat: gradio link with nestjs
    
    * feat: gradio link with nestjs
    
    * fix: script to fix gradio package
    
    * build: deploy.yml + script to deploy private files
    
    * fix: now auto-creating assets folder if not existing
    
    * build: modified index.js to main.js

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors( {
+    origin: 'http://localhost:5173',
+  })
+  await app.listen(5000);
 }
 bootstrap();
