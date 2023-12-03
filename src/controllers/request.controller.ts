2c8644e865097a3208fbe5fcbb643c759c9bd476
PhantHive <phantomhive72@yahoo.fr>
2023-12-03 17:18:15 +0100

    feat: getFiles as buffer from prediction

--- src/controllers/request.controller.ts
+++ src/controllers/request.controller.ts
@@ -1,4 +1,6 @@
 import {Controller, Post, Body, Res, HttpStatus} from '@nestjs/common';
+import { getFiles } from './archive/getFiles';
+
 export const importDynamic = new Function('modulePath', 'return import(modulePath)');
 const EventSource = require('eventsource');
 globalThis.EventSource = EventSource;
@@ -9,8 +11,17 @@ export class RequestController {
   async receiveString(@Body('data') data: string, @Res() res): Promise<void> {
     console.log('Received string: ', data);
     const { client } = await importDynamic('@gradio/client');
-    const C = await client('https://phanthive-phanthive-bigbrain.hf.space/--replicas/9278f/', {})
+    const C = await client('https://phanthive-phanthive-bigbrain.hf.space/--replicas/9q8sg/', {})
     const result = await C.predict("/predict", [data])
-    res.status(HttpStatus.OK).json({ prediction: result.data[0]})
+    const files = await getFiles(result.data[0]);
+    console.log(files);
+
+    if (typeof files === 'string') {
+      // No resources were found
+      res.status(HttpStatus.NOT_FOUND).json({ message: files });
+    } else {
+      // Files were found
+      res.status(HttpStatus.OK).json({ prediction: result.data[0], files: files });
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   app.enableCors( {
     origin: 'http://localhost:5173',
   })
-  await app.listen(5000);
+  await app.listen(42125);
 }
 bootstrap();
