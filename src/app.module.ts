ceb5709b5db055d182e4f12eecd192297710c829
Zakaria <49699418+PhantHive@users.noreply.github.com>
2023-12-03 22:04:01 +0100

    feat: Saving data from big brain app to Mongo (#6)
    * feat: save data controller
    
    now handling data from big brain app to be saved on mongo
    
    * feat: added new item in request model
    
    * chore: linter modif
    
    * feat: added response status in try catch
    
    * test: unit test for data controller
    
    * feat: better error handling
    
    ---------
    
    Co-authored-by: PCK <charles.kantane@outlook.fr>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './app.service';
 import { RequestController } from './controllers/request.controller';
+import {SaveDataController} from "./controllers/saveData.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController, RequestController],
+  controllers: [AppController, RequestController, SaveDataController],
   providers: [AppService],
 })
 export class AppModule {}
