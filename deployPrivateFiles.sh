09f8a48ce5f36451a6a3ed162d630779bb04861b
PhantHive <phantomhive72@yahoo.fr>
2023-12-06 01:07:13 +0100

    build: fix deploy files

--- deployPrivateFiles.sh
+++ deployPrivateFiles.sh
@@ -4,20 +4,17 @@
 SOURCE_DIR="/home/Phearion/sushiPrivate"
 DEST_DIR="$(pwd)/src/assets"
 
-# Print out the contents of the source directory
-echo "Contents of the source directory before copy:"
-ls -la "$SOURCE_DIR"
-
 # if the destination directory does not exist, create it
 if [ ! -d "$DEST_DIR" ]; then
   mkdir -p "$DEST_DIR"
 fi
 
-# After copy
-echo "Contents of the destination directory after copy:"
-ls -la "$DEST_DIR"
-
-# Copy files and directories recursively except .env
-rsync -av --exclude=".env" "$SOURCE_DIR"/* "$DEST_DIR"
 # Add .env to root dist
 cp "$SOURCE_DIR/.env" "$(pwd)/.env"
+
+# Copy everything inside the source directory to the destination directory (except .env)
+cp -r "$SOURCE_DIR/." "$DEST_DIR"
+
+# Remove the .env file from the destination directory
+rm "$DEST_DIR/.env"
+
