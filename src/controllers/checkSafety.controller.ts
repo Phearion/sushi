d5b88864f30bd2af5acfd07e412cfd4abdf70d73
PhantHive <phantomhive72@yahoo.fr>
2024-01-22 17:48:18 +0100

    chore: updated controller for docker

--- src/controllers/checkSafety.controller.ts
+++ src/controllers/checkSafety.controller.ts
@@ -8,7 +8,6 @@ import {
 } from '@nestjs/common';
 import { spawn } from 'child_process';
 import * as path from 'path';
-import * as os from 'os';
 
 @Controller('checkRequest')
 export class CheckSafetyController {
@@ -28,8 +27,7 @@ export class CheckSafetyController {
             const result = await new Promise<string>((resolve, reject) => {
                 let result = '';
                 console.log("Requête en cours d'analyse:", request);
-                const pythonCommand =
-                    os.platform() === 'win32' ? 'python' : 'python3';
+                const pythonCommand = 'python'; // using python because docker env
                 const process = spawn(pythonCommand, [
                     pythonScriptPath,
                     request,
